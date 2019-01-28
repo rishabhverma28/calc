2bb3944973785be9c7da4d02be79ce26308992fc
Rishabh Verma <rishabhverma.2806@gmail.com>
2019-01-28 10:31:25 +0530

    Updates

---
+++ precache-manifest.7446c5ef5b65082049a6df4ef5045dd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "364ef4b3b164907ef7ba",
+    "url": "/react-calc/static/js/runtime~main.364ef4b3.js"
+  },
+  {
+    "revision": "4de6470229e46940594a",
+    "url": "/react-calc/static/js/main.4de64702.chunk.js"
+  },
+  {
+    "revision": "1b8c1cc74ee3d61a5e5e",
+    "url": "/react-calc/static/js/1.1b8c1cc7.chunk.js"
+  },
+  {
+    "revision": "4de6470229e46940594a",
+    "url": "/react-calc/static/css/main.3bbfd4a6.chunk.css"
+  },
+  {
+    "revision": "c652db039b20f4a74983daeae546d1f5",
+    "url": "/react-calc/index.html"
+  }
+];
